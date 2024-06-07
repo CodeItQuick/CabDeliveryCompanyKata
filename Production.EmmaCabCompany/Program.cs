7d4eb35c3794c0790892434f86eaaac01103a655
CodeItQuick <evanontario@hotmail.com>
2024-06-07 09:02:29 -0300

    more fixes

--- Production.EmmaCabCompany/Program.cs
+++ Production.EmmaCabCompany/Program.cs
@@ -25,14 +25,5 @@ public class Program
             cabs, 
             new Customer("Diane", "1 Fulton Drive", "1 University Avenue"),
             cabCompanyPrinter);
-    
-            
-        // This emma's cab company will pickup and deliver a customer.
-        // Emma's Cab picked up Darrell at 1 Fulton Drive
-        // Emma's Cab dropped off Darrell at 1 University Avenue
-            
-        // This emma's cab company will pickup and deliver a customer.
-        // Emma's Cab picked up Diane at 1 Fulton Drive
-        // Emma's Cab dropped off Diane at 1 University Avenue
     }
 }
