9214d9be1cbfce0319f17d5e486ac9dc50f19b14
CodeItQuick <evanontario@hotmail.com>
2024-06-09 17:27:51 -0300

    move file

--- Tests.CabDeliveryCompanyKata/AcceptanceTests.cs
+++ Tests.CabDeliveryCompanyKata/AcceptanceTests.cs
@@ -29,16 +29,3 @@ public class AcceptanceTests
     }
     
 }
-
-public class FakeCabCompanyWriter : ICabCompanyWriter
-{
-    private int currentCommand = 0;
-    public List<string> CommandList { get; set; } = new List<string>();
-    public string? ReadLine()
-    {
-        var current = CommandList[currentCommand];
-        currentCommand += 1;
-        return current;
-    }
-
-}
