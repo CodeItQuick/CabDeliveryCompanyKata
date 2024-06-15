f788af7a7bb095f517f75099fa21076d42d7bfaf
CodeItQuick <evanontario@hotmail.com>
2024-06-15 16:12:56 -0300

    clarify

--- Production.EmmaCabCompany/UserInterface.cs
+++ Production.EmmaCabCompany/UserInterface.cs
@@ -42,13 +42,13 @@ public class UserInterface
             // also in the command pattern
             // command -> menu option string, -> execute with parameters
             _cabCompanyPrinter.WriteLine("0. Exit");
-            _cabCompanyPrinter.WriteLine("1. Add New Cab Driver");
-            _cabCompanyPrinter.WriteLine("2. Remove Cab Driver"); // TODO: weird tricks here could cause bugs
-            _cabCompanyPrinter.WriteLine("3. Send Cab Driver Ride Request");
-            _cabCompanyPrinter.WriteLine("4. (External) Cab Notifies Passenger Picked Up");
-            _cabCompanyPrinter.WriteLine("5. (External) All Cab Notifies Passenger Dropped Off");
-            _cabCompanyPrinter.WriteLine("6. Cancel Cab Driver Fare");
-            _cabCompanyPrinter.WriteLine("7. (External) Customer Request Ride");
+            _cabCompanyPrinter.WriteLine("1. (Incoming Call) Add New Cab Driver");
+            _cabCompanyPrinter.WriteLine("2. (Incoming Call) Remove Cab Driver"); // TODO: weird tricks here could cause bugs
+            _cabCompanyPrinter.WriteLine("3. (Outgoing Call) Send Cab Driver Ride Request");
+            _cabCompanyPrinter.WriteLine("4. (Incoming Call) Cab Notifies Passenger Picked Up");
+            _cabCompanyPrinter.WriteLine("5. (Incoming Call) All Cab Notifies Passenger Dropped Off");
+            _cabCompanyPrinter.WriteLine("6. (Incoming Call) Cancel Cab Driver Fare");
+            _cabCompanyPrinter.WriteLine("7. (Incoming Call) Customer Request Ride");
             var lineEntered = _cabCompanyReader.ReadLine();
 
             _cabCompanyPrinter.WriteLine($"You selected: {lineEntered}");
