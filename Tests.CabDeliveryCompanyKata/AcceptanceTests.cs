68dc868106cf38f0ceff392240c71b8cc927c35b
CodeItQuick <evanontario@hotmail.com>
2024-06-15 07:42:26 -0300

    fixes

--- Tests.CabDeliveryCompanyKata/AcceptanceTests.cs
+++ Tests.CabDeliveryCompanyKata/AcceptanceTests.cs
@@ -26,5 +26,27 @@ public class AcceptanceTests
         Assert.Equal("Evan's Cab picked up Emma at 1 Fulton Drive.", cabCompanyPrinter.Retrieve(36));
         Assert.Equal("Evan's Cab dropped off Emma at 1 Destination Lane.", cabCompanyPrinter.Retrieve(37));
     }
+    [Fact]
+    public void TheCabCompanyCanPickupTwoCustomerAtAnAddress()
+    {
+        FakeCabCompanyPrinter cabCompanyPrinter = new FakeCabCompanyPrinter();
+        FakeCabCompanyReader cabCompanyReader = new FakeCabCompanyReader()
+        {
+            CommandList = new List<string>()
+            {
+                "1",
+                "6",
+                "3",
+                "4",
+                "0"
+            }
+        };
+        var mockFileReadWriter = new MockFileReadWriter();
+        var userInterface = new UserInterface(cabCompanyPrinter, cabCompanyReader, mockFileReadWriter, mockFileReadWriter);
+        userInterface.Run();
+        
+        Assert.Equal("Evan's Cab picked up Emma at 1 Fulton Drive.", cabCompanyPrinter.Retrieve(36));
+        Assert.Equal("Evan's Cab dropped off Emma at 1 Destination Lane.", cabCompanyPrinter.Retrieve(37));
+    }
     
 }
