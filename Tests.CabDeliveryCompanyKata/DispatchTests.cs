a80ec34defcd76a3a4a8a9b57215e8ee3d3fc975
CodeItQuick <evanontario@hotmail.com>
2024-06-16 10:48:01 -0300

    fill in more tests

--- Tests.CabDeliveryCompanyKata/DispatchTests.cs
+++ Tests.CabDeliveryCompanyKata/DispatchTests.cs
@@ -61,6 +61,41 @@ public class DispatchTests
         Assert.Equal(4, cabCompanyPrinter.CountMessages());
     }
     [Fact]
+    public void CanPickupTwoCustomersWithTwoCabsSecondOrdering()
+    {
+        var cabCompanyPrinter = new FakeCabCompanyPrinter();
+        var cabs = new Cabs("Dan's Cab", cabCompanyPrinter, 20);
+        var cabTwo = new Cabs("Evan's Cab", cabCompanyPrinter, 20);
+        var dispatch = new Dispatch(cabCompanyPrinter);
+        var customer = new Customer(
+            "Emma", 
+            "1 Fulton Drive", 
+            "1 Final Destination Lane");
+        var customerTwo = new Customer(
+            "Lisa", 
+            "2 Fulton Drive", 
+            "2 Final Destination Lane");
+        dispatch.AddCab(cabs);
+        dispatch.AddCab(cabTwo);
+        dispatch.RideRequest(customer);
+        dispatch.RideRequest(customerTwo);
+        dispatch.PickupCustomer(customer);
+        dispatch.PickupCustomer(customerTwo);
+
+        dispatch.DropOffCustomers();
+        
+        // TODO: Broken, Dan's cab doesn't pick anyone up
+        Assert.Contains("Evan's Cab picked up Lisa at 2 Fulton Drive.", 
+            cabCompanyPrinter.List());
+        Assert.Contains("Dan's Cab picked up Emma at 1 Fulton Drive.", 
+            cabCompanyPrinter.List()); 
+        Assert.Contains("Evan's Cab dropped off Lisa at 2 Final Destination Lane.", 
+            cabCompanyPrinter.List()); 
+        Assert.Contains("Dan's Cab dropped off Emma at 1 Final Destination Lane.", 
+            cabCompanyPrinter.List()); 
+        Assert.Equal(4, cabCompanyPrinter.CountMessages());
+    }
+    [Fact]
     public void CannotPickupCustomerIfNotAvailable()
     {
         var cabCompanyPrinter = new FakeCabCompanyPrinter();
